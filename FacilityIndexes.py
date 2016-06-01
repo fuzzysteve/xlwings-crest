6dec55d69f8393f1ad2b66f1f7cbf53820e2e30d
Steve Anderson / Steve Ronuken <steve@fuzzysteve.co.uk>
2016-06-01 18:19:48 +0100

    Create FacilityIndexes.py

---
+++ FacilityIndexes.py
@@ -0,0 +1,27 @@
+@xw.sub
+def facilityIndexs():
+    url="https://public-crest.eveonline.com/industry/systems/"
+    wb = Workbook.caller()
+    sheet = xw.Sheet('Indexes')
+    
+    
+    r=requests.get(url)
+    
+    sheetdata=[]
+    sheetdata.append(["System Name","System ID","Manufacturing","TE","ME","Copy","Invention"])
+    
+    if r.status_code == 200:
+        for item in r.json()["items"]:
+            rowdata=[]
+            indexes={}
+            for index in item["systemCostIndices"]:
+                indexes[index["activityID"]]=index["costIndex"]
+            rowdata.append(item["solarSystem"]["name"])
+            rowdata.append(item["solarSystem"]["id"])
+            rowdata.append(indexes[1])
+            rowdata.append(indexes[3])
+            rowdata.append(indexes[4])
+            rowdata.append(indexes[5])
+            rowdata.append(indexes[8])
+            sheetdata.append(rowdata)
+    Range('A1').value=sheetdata
